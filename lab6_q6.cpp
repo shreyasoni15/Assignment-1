0389c4a47d9a34b8fe2363fd72d9749da94910fc
shreyasoni15 <42139513+shreyasoni15@users.noreply.github.com>
2018-09-24 22:16:03 +0530

    Update lab6_q6.cpp

--- lab6_q6.cpp
+++ lab6_q6.cpp
@@ -1,6 +1,12 @@
+//WAP to find Sum of even and odd and their squares as well in a given range of  2 numbers
+
+
+//library
 #include <iostream>
 
 using namespace std;
+
+/*Write a function that outputs the sum of all even numbers between firstNum and secondNum*/
 
 int sumEvenNumbers(int firstNum, int secondNum)
 {   int sumE=0;
@@ -16,6 +22,8 @@ int sumEvenNumbers(int firstNum, int secondNum)
     }
     return sumE;
 }
+//Write a function that outputs the sum of all odd numbers between firstNum and secondNum
+
 int sumOddNumbers(int firstNum, int secondNum)
 {   int sumO=0;
     if(firstNum%2!=0)
@@ -31,54 +39,79 @@ int sumOddNumbers(int firstNum, int secondNum)
     return sumO;
 }
 
-
+//Write a function that outputs the sum of the square of the odd numbers between firstNum and secondNum
 int sumSquareOddNumbers(int firstNum, int secondNum)
 {   int sumOS=0;
     if(firstNum%2!=0)
     {
-        for(int i=firstNum;i<=secondNum;i+=2)
+        int i=firstNum;
+        while(i<=secondNum)
+        {
+
             sumOS+=(i*i);
+            i+=2;
+        }
     }
     else if(firstNum%2==0)
     {
-        for(int i=firstNum+1;i<=secondNum;i+=2)
-            sumOS+=(i*i);
+        int i=firstNum+1;
+            while(i<=secondNum)
+            {
+
+                sumOS+=(i*i);
+                i+=2;
+            }
     }
     return sumOS;
 }
 
-
+//Write a function that outputs the sum of the square of the odd numbers between firstNum and secondNum
 int sumSquareEvenNumbers(int firstNum, int secondNum)
 {   int sumES=0;
     if(firstNum%2==0)
     {
-        for(int i=firstNum;i<=secondNum;i+=2)
+        int i=firstNum;
+        while(i<=secondNum)
+        {
+
             sumES+=(i*i);
+            i+=2;
+        }
     }
     else if(firstNum%2!=0)
     {
-        for(int i=firstNum+1;i<=secondNum;i+=2)
-            sumES+=(i*i);
+            int i=firstNum+1;
+            while(i<=secondNum)
+            {
+
+                sumES+=(i*i);
+                i+=2;
+            }
     }
     return sumES;
 }
-
+//main 
 int main()
 {
-    int sumEven,sumOdd,sumSquareEven,sumSquareOdd,n1,n2;
+    int sumEven,sumOdd,sumSquareEven,sumSquareOdd,n1,n2; //variables
 
+    
+    //input
     cout<<"Enter first number"<<" "<<"\n";
     cin>>n1;
 
 
     cout<<"Enter second number"<<" "<<"\n";
     cin>>n2;
-
+    
+    //function calls
+    
     sumEven=sumEvenNumbers(n1,n2);
     sumOdd=sumOddNumbers(n1,n2);
     sumSquareEven=sumSquareEvenNumbers(n1,n2);
     sumSquareOdd=sumSquareOddNumbers(n1,n2);
 
+    //output
     cout<<"the values are:"<<"\n";
 
     cout<<"sumEven: "<<sumEven<<"\n";
